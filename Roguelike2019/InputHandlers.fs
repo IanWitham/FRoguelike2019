a2fa0fae5108ba738884a6d73b5e6094be73cc6d
Ian Witham <witham.ian@gmail.com>
2019-06-21 22:25:19 +1200

    Gameloop and player movement

---
+++ Roguelike2019/InputHandlers.fs
@@ -0,0 +1,17 @@
+﻿module InputHandlers
+
+open System
+open Microsoft.Xna.Framework.Input
+
+let folder (playerX, playerY) (key : SadConsole.Input.AsciiKey) =
+    match key.Key with
+        | Keys.Left -> (playerX - 1, playerY)
+        | Keys.Right -> (playerX + 1, playerY)
+        | Keys.Up -> (playerX, playerY - 1)
+        | Keys.Down -> (playerX, playerY + 1)
+        | Keys.None -> (playerX, playerY)
+
+let handleKeys keys gameState =
+    List.fold folder gameState keys
+
+
--- Roguelike2019/Program.fs
+++ Roguelike2019/Program.fs
@@ -2,31 +2,56 @@
 
 open System
 open SadConsole
-open Microsoft.Xna.Framework;
-open Microsoft.Xna.Framework.Graphics;
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+open InputHandlers
 
 //let Console = SadConsole.Console
 
 let width = 80
 let height = 25
 
+let playerChar = '@'
+
+let mutable playerPosition = (width / 2, height / 2)
+
+let kb = new SadConsole.Input.Keyboard()
+
+
+
 let Init() : unit = 
     // Any startup code for your game. We will use an example console for now
     let startingConsole = SadConsole.Global.CurrentScreen;
-    startingConsole.FillWithRandomGarbage();
-    startingConsole.Fill (Rectangle (3, 3, 27, 5), System.Nullable(Color.Red), System.Nullable(Color.Black), System.Nullable(0), System.Nullable(SpriteEffects.None)) |> ignore
-    startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
+
+    //startingConsole.FillWithRandomGarbage();
+    //startingConsole.Fill (Rectangle (3, 3, 27, 5), System.Nullable(Color.Red), System.Nullable(Color.Black), System.Nullable(0), System.Nullable(SpriteEffects.None)) |> ignore
+    //startingConsole.Print(6, 5, "Hello from SadConsole", ColorAnsi.CyanBright);
+    
+    startingConsole.SetGlyph(width / 2, height / 2, (int) playerChar);
+
+let Update (gt : GameTime) : unit = 
+    kb.Update(gt)
+    let keyList = List.ofSeq kb.KeysPressed
+    playerPosition <- handleKeys keyList playerPosition
+
+let Draw (gt : GameTime) : unit =
+    let startingConsole = SadConsole.Global.CurrentScreen;
+    let playerX, playerY = playerPosition
+    startingConsole.Fill(System.Nullable(Color.White), System.Nullable(Color.Black), System.Nullable(0)) |> ignore
+    startingConsole.SetGlyph(playerX, playerY, (int) playerChar)
 
 [<EntryPoint>]
 let main argv =
     SadConsole.Game.Create(width, height)    
 
-    // Hook the start event so we can add consoles to the system.
-    SadConsole.Game.OnInitialize <- new Action(Init);
+    SadConsole.Game.OnInitialize <- new Action(Init)
+    SadConsole.Game.OnUpdate <- new Action<GameTime>(Update)
+    SadConsole.Game.OnDraw <- new Action<GameTime>(Draw)
             
     // Start the game.
     SadConsole.Game.Instance.Run();
     SadConsole.Game.Instance.Dispose();
+
     printfn "Hello World from F#! %d" width
     0 // return an integer exit code
 
