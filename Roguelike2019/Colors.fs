46f8ee97c5b1338a6ca12b4f1c79732f56798b59
Ian Witham <witham.ian@gmail.com>
2019-06-24 21:57:19 +1200

    Remove unneccesary "new" keywords

--- Roguelike2019/Colors.fs
+++ Roguelike2019/Colors.fs
@@ -2,6 +2,6 @@
 
 open Microsoft.Xna.Framework
 
-let DarkWall = new Color(0, 0, 100)
-let DarkGround = new Color(50, 50, 150)
+let DarkWall = Color(0, 0, 100)
+let DarkGround = Color(50, 50, 150)
 
