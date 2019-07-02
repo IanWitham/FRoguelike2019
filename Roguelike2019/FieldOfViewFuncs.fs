cc95231121c76bd32d32e98015e562025ec6ca9a
Ian Witham <witham.ian@gmail.com>
2019-07-02 22:14:13 +1200

    Code tidy up

--- Roguelike2019/FieldOfViewFuncs.fs
+++ Roguelike2019/FieldOfViewFuncs.fs
@@ -1,22 +1,5 @@
 ﻿module FieldOfView
 
-open GameTypes
-open MapTypes
-
-let private transformOctant row col octant =
-    match octant with
-    | 0 -> ( col, -row )
-    | 1 -> ( row, col )
-    | 2 -> ( row, col )
-    | 2 -> ( row, col )
-
-type private octantDefinition = {
-    direction:(int * int)
-    range:(int * int)
-}
-
-
-
 let Octant maxDistance = 
     List.map (fun y -> List.map (fun x -> (x, y)) [0..y]) [0..maxDistance]
 
